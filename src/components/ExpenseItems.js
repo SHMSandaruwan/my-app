79a4e8f3cf1e8c9138a619f9ea953a799082022b
SHMSandaruwan <47217961+SHMSandaruwan@users.noreply.github.com>
2021-05-13 01:59:51 +0530

    componnet

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,17 +1,22 @@
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
+
 
 function ExpenseItme(props) {
     // const expenseDate = new Date(2021, 4,10);
     // const expenseTitle = 'Car Insurance';
     // const expenseAmount = 294.67;
+
     return (
-        <div className="expense-item">
-            <div>{props.date.toDateString()}</div>
+        <Card className="expense-item">
+            {/* <div>{props.date.toDateString()}</div> */}
+            <ExpenseDate date = {props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>            
-        </div>        
+        </Card>        
     );
 }
 
